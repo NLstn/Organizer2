8a29cfff65adc7675ddfa004ec91cc2348932d79
Niklas Lahnstein <niklas.lahnstein@gmail.com>
2020-10-06 16:52:47 +0000

    Remove UI Mode for now

--- src/main/java/com/nlstn/App.java
+++ src/main/java/com/nlstn/App.java
@@ -1,20 +1,12 @@
 package com.nlstn;
 
-import com.nlstn.ui.Window;
-
 /**
  * Hello world!
  */
 public final class App {
 
-    private Window window;
-
-    private App() {
-        window = new Window();
-    }
-
     public static void main(String[] args) {
-        new App();
+
     }
 
 }
